3def0f1f12a35c78659c07f3b1f6c6f306641851
Josue516 <josue516x@gmail.com>
2025-09-03 00:11:33 -0500

    Creacion de la clase Snake

---
+++ SnakeGame/src/Codigo/Snake.java
@@ -0,0 +1,53 @@
+package Codigo;
+
+import java.util.ArrayList;
+import processing.core.PVector;
+
+public class Snake {
+	//ALMACENAN LAS POSICIONES DEL SNAKE
+	public ArrayList<Integer> posX = new ArrayList<>();
+	public ArrayList<Integer> posY = new ArrayList<>();
+	
+	public boolean alive = true;
+	
+	//POSICIONES POR DONDE EMPIEZAN
+	private final PVector initialPosition1;
+	private final PVector initialPosition2;
+
+	public int r,g,b;
+	
+	public Snake(int r, int g, int b, PVector initial1, PVector initial2) {
+		this.r = r;
+		this.g = g;
+		this.initialPosition1 = initial1;
+		this.initialPosition2 = initial2;
+		
+		restart();
+	}
+	
+	//SUMA COORDENADAS A LA SERPIENTE PARA QUE SE MUEVA, DESPUES ELIMINA LA ULTIMA
+	//PARA HACER PARECER EL MOVIMIENTO
+	public void mover(int x, int y) {
+		posX.add(0, posX.get(0)+x);
+		posY.add(0, posY.get(0)+y);
+		
+		posX.remove(posX.size()-1);
+		posY.remove(posY.size()-1);
+	}
+	//PARA HACER MAS GRANDE A LA SERPIENTE
+	public void comer() {
+		posX.add(posX.get(posX.size() - 1));
+		posY.add(posY.get(posY.size() - 1));
+	}
+	//REINICIAR TODO AL COMIENZO
+	public void restart() {
+		posX.clear();
+		posY.clear();
+		alive=true;
+		
+		posX.add((int)initialPosition1.x);
+		posY.add((int)initialPosition1.y);
+		posX.add((int)initialPosition2.x);
+		posY.add((int)initialPosition2.y);
+	}
+}
